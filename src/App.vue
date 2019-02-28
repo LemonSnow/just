1575a163aa33c436d3095d15be2852306b7f2d5e
Lemon <ovo20999@163.com>
2019-02-28 23:47:47 +0800

    the first

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+<div id="app" >
+  <div >
+    <people :name="item.name" :age="item.age" v-for="item in peoples" :key="item.id" />
+  </div>
+
+
+<!-- <img src="@/assets/logo.png" alt="">
+<people name="韩宇琪"></people> -->
+</div>
+  <!-- <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+</template>
+
+<script>
+// let peophoto=document.getElementById('cmg')
+// let photoDom=document.createElement('img')
+//     photoDom.className='item.photo'
+//     peophoto.appendChild(photoDom)
+import people from './people.vue'
+// import lemon from './assets/lemon.jpg'
+// import jiajia from './assets/2.png'
+// import qiqi from './assets/3.png'
+// import feifei from './assets/4.jpg'
+export default {
+  components:{
+    people
+  },
+  data () {
+    return {
+      peoples:[
+        {
+          id: 1,
+          name:'lemon',
+          age:'18',
+          photo:'lemon'
+        },
+        {
+          id: 2,
+          name:'jiajia',
+          age:'17',
+          photo:'jiajia'
+        },
+        {
+          id: 3,
+          name:'qiqi',
+          age:'20',
+          photo:'qiqi'
+        },
+        {
+          id: 4,
+          name:'feiafei',
+          age:'19',
+          photo:'feifei'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/people.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+     <!-- <h1>王葳蕤</h1> -->
+     <img src="@/assets/2.png" alt="">
+     <h1>name:{{name}}</h1> 
+     <h4>age:{{age}}</h4>
+     </div> 
+</template>
+
+<script>
+export default {
+    props:[
+      'name',
+      'age'
+    ]
+    // name="wangning"
+}
+</script>
+<style>
+
+</style>
